0f47c9ddb0506a8ed170c5ee11ca7bfb034aea56
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-04-23 11:30:37 +0700

    menambahkan conditional statement

--- singleLinklist.cpp
+++ singleLinklist.cpp
@@ -46,5 +46,17 @@ public:
 
         while (current != NULL && nim > current->noMhs)
         {
+             if (nim== current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
            
 };
