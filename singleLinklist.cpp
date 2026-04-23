318eb0c78be382172d5174716f6bb73c3dc4ca6d
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-04-23 11:07:47 +0700

    menambahkan class linkedlist

--- singleLinklist.cpp
+++ singleLinklist.cpp
@@ -5,5 +5,18 @@ using namespace std;
 class Node
 {
 public:
-    
+    int noMhs;
+    Node *next;
 };
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+}
